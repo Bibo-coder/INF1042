a2a1b6506a07a688cc23413c79dc16347a6f6718
Bibo-coder <mikemoumneh@gmail.com>
2026-03-17 12:36:05 -0400

    T

---
+++ exercices/module 3/Tâche03b - q4.py
@@ -0,0 +1,10 @@
+mot = input("Entrez un mot : ")
+
+print("mot * 3 =", mot * 3)
+
+print("mot + '!' =", mot + "!")
+
+if "a" in mot:
+    print("Le mot contient la lettre 'a'")
+else:
+    print("Le mot ne contient pas la lettre 'a'")
