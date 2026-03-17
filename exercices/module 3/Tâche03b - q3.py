03d54d219466d91f59331abd14a1618eda0eaa20
Bibo-coder <mikemoumneh@gmail.com>
2026-03-17 12:29:06 -0400

    T

---
+++ exercices/module 3/Tâche03b - q3.py
@@ -0,0 +1,10 @@
+def sortie_permise(temperature, pluie)
+    return temperature >= 15 and pluie == "non"
+
+temperature = float(input("Entrez la température : "))
+pluie = input("Est-ce qu'il pleut ? (oui/non) : ")
+
+if sortie_permise(temperature, pluie):
+    print("Sortie permise")
+else:
+    print("On reste dedans")
