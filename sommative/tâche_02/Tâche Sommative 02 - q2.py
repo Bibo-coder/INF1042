64737a4f9813b2da42c581dbf3986cb45f79e824
Bibo-coder <mikemoumneh@gmail.com>
2026-03-30 19:17:25 -0400

    T

---
+++ sommative/tâche_02/Tâche Sommative 02 - q2.py
@@ -0,0 +1,12 @@
+# Ibrahim Moumneh
+# Demander la température en Celsius
+celsius = float(input("Entrez une température en degrée Celsius : "))
+
+# Conversion
+fahrenhit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+# Affichage des résultats
+print("Résultats :")
+print("Température en fahrenhit :", fahrenhit)
+print("Température en kelvin :", kelvin)
